c3a6b7b6fd4f37423e91f0f64bf509284b308ec4
nikohernandiz <nikohernandiz@gmail.com>
2016-11-14 13:30:48 -0800

    Create ie.go

---
+++ ie.go
@@ -0,0 +1,21 @@
+import package
+
+
+//Take user input and identify the quote in the knowledge base
+function idQuote {
+
+
+}
+
+//logic for verifying the correct line of dialogue
+function idLine {
+
+
+}
+
+//logic for verifying the correct character speaking
+function idCharacter {
+
+
+
+}
